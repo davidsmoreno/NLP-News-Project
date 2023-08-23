b5e43c575bfd06b7763371f87390f83009fe970b
davidsmoreno <davidsantiagomoreno@gmail.com>
2023-08-23 15:02:00 -0500

    Add: List of Neighbors

--- Data_Exploring.py
+++ Data_Exploring.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import re
+import spacy
 
-# Read the data
+#Read the data
 data = pd.read_csv("data.csv")
+neighbors=pd.read_csv("NeighborhoodsUsa.csv")
 
+ny_neighbors=neighbors[neighbors['city_name']=='New York']
+list_ny_neighbors=ny_neighbors['neighborhood']
+list_ny_neighbors=list(list_ny_neighbors)
 
 ##Clean data and extract only the date in a string
 
@@ -30,3 +35,59 @@ def extract_date(text):
 
 data["Time"] = data["Text"].apply(extract_time)
 data["Date"] = data["Text"].apply(extract_date)
+
+
+# Load the English language model
+nlp = spacy.load("en_core_web_sm")
+
+text1=data['Text'][0]
+doc=nlp(text1)
+
+phrases = []
+
+for sent in doc.sents:
+    sent_phrases = [token.text for token in sent]
+    phrases.append(" ".join(sent_phrases))
+
+
+list_ny_neighbors=[string.upper() for string in list_ny_neighbors]
+
+
+# adjacent_phrases = []
+
+# for i, phrase in enumerate(phrases):
+#     for j, word in enumerate(list_ny_neighbors):
+#         if word in phrase:
+#             adjacent = " ".join([phrases[i-1], phrase, phrases[i+1]])
+#             adjacent_phrases.append((i, j, adjacent))
+
+# df1 = pd.DataFrame(adjacent_phrases, columns=["PhraseIndex", "SearchWordIndex", "AdjacentPhrase"])
+
+
+adjacent_phrases = []
+
+for i, phrase in enumerate(phrases):
+    for j, word in enumerate(list_ny_neighbors):
+        if word in phrase.split():
+            if i > 0 and i < len(phrases) - 1:
+                adjacent = " ".join([phrases[i-1], phrase, phrases[i+1]])
+                adjacent_phrases.append((i, j, adjacent))
+
+df1 = pd.DataFrame(adjacent_phrases, columns=["PhraseIndex", "SearchWordIndex", "AdjacentPhrase"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
